b26de013329be19703173fb54bd3d2835e3fea3a
Marcelo Nicolas Romani <nicolasromani.dev@gmail.com>
2024-07-19 10:30:27 -0300

    update tailwind colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,19 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+   content: [
+      "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+   ],
+   theme: {
+      extend: {
+         colors: {
+            "brown": '#1c1c1c',
+            "light-brown": '#9d7d28'
+         }
+      },
+   },
+   plugins: [],
 };
 export default config;
