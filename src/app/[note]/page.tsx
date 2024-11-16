53b3c5e6d634734b1d331b45e2cb13a665e21cbf
Marcelo Nicolas Romani <nicolasromani.dev@gmail.com>
2024-11-16 12:04:02 -0300

    refactor note-page

--- src/app/[note]/page.tsx
+++ src/app/[note]/page.tsx
@@ -1,5 +1,26 @@
+'use client'
+
 import Note from "@/components/note/Note";
+import useNotes from "@/hooks/useNotes";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+import { Note as NoteType } from "@/types/note";
 
 export default function Page() {
-   return <Note/>
+   const path = usePathname()
+   const { notes,  getNote } = useNotes()
+   const [note, setNote] = useState<NoteType | null>(null)
+
+   useEffect(() => {
+      if(!notes) return
+      const notePathID = path.split('/note-')[1] 
+      const note = getNote(notePathID)
+      setNote(note)
+   },[notes])
+
+   return (
+      <>
+         {(note) && <Note note={note}/>}
+      </>
+   )
 }
--- src/components/note/Note.tsx
+++ src/components/note/Note.tsx
@@ -2,50 +2,40 @@
 
 import useNotes from "@/hooks/useNotes"
 import type { Note as NoteType } from "@/types/note"
-import { usePathname } from "next/navigation"
-import { useEffect, useState } from "react"
 import NoteTitle from "./NoteTitle"
 import type { UpdateNoteFields } from "@/types/context/notesContext"
 import createNoteDate from "@/lib/createNoteDate"
 import Content from "./Content"
 
-export default function Note() {
-   const path = usePathname()
-   const { notes,  getNote, updateNote } = useNotes()
-   const [note, setNote] = useState<NoteType | null>(null)
+interface Props {
+   note: NoteType
+}
 
-   useEffect(() => {
-      if(!notes) return
-      const notePathID = path.split('/note-')[1] 
-      setNote(getNote(notePathID))
-   },[notes])
+export default function Note({ note }: Props) {
+   const { updateNote } = useNotes()
 
    function onUpdateNote(data: UpdateNoteFields) {
       return updateNote((note as NoteType).id, data)
    }
-
+   
    return (
       <>
-         {(note) && 
-            <>
-               <p className="text-gray-300 text-center text-xs">
-                  {
-                     createNoteDate(note.createAt)
-                  }
-               </p>
+         <p className="text-gray-300 text-center text-xs">
+            {
+               createNoteDate(note.createAt)
+            }
+         </p>
 
-               <NoteTitle
-                  title={note.title.content}
-                  emoji={note.title.emoji}
-                  onUpdateNote={onUpdateNote}
-               />
+         <NoteTitle
+            title={note.title.content}
+            emoji={note.title.emoji}
+            onUpdateNote={onUpdateNote}
+         />
                
-               <Content 
-                  content={note.content}
-                  onUpdateNote={(content) => onUpdateNote({ content })}
-               />
-            </>
-         }
+         <Content 
+            content={note.content}
+            onUpdateNote={(content) => onUpdateNote({ content })}
+         />
       </>
    )
 }
