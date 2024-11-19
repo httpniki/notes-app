88e6746bc064db9303f7b80c1e7d2eab55e2e60b
Marcelo Nicolas Romani <nicolasromani.dev@gmail.com>
2024-11-19 09:21:49 -0300

    fix(note-page): add a zero for single-digit minutes in the timestamps

--- src/lib/createNoteDate.ts
+++ src/lib/createNoteDate.ts
@@ -9,8 +9,7 @@ export default function createNoteDate(noteDate: Note['createAt']) {
    const month = date.monthShort
    const day = date.day
    const year = date.year
-   const time = `${date.hour}:${date.minute}`
-
+   const time = `${date.hour}:${date.minute < 10 ? '0' + date.minute : date.minute}`
 
    return `${month} ${day}, ${year} at ${time}`
 }
