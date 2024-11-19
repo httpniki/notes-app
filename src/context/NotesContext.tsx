d5526011f7b3ae61f3edd725dcb7c7e9a852f3f9
Marcelo Nicolas Romani <nicolasromani.dev@gmail.com>
2024-11-19 09:42:09 -0300

    fix types

--- src/context/NotesContext.tsx
+++ src/context/NotesContext.tsx
@@ -125,7 +125,7 @@ export default function NotesContextProvider({ children }: ContextProps) {
             updateNote,
             deleteNote,
             searchParams,
-            setSearchParams
+            setSearchParams: setSearchParams as ContextValue['setSearchParams'] ,
          }}>
          {children}
       </notesContext.Provider>
